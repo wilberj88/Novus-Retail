bd981e3639831136c2a612f7e0200c4fcf4de604
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-10-22 23:16:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide", page_title="Novus Retail", page_icon="🛒")
 
 st.title('Novus Retail 🛒 Automated Cycle')
 
-st.header("Mandos (5) ⚙️ & Atentos (2)🤖")
+st.header("Mandos (5) ⚙️ & Atentos (4)🤖")
 st.subheader("Demand, Competence, Stock, Sales & Optimal Ubications 🎯")
-st.subheader("New Sales & Confirmn, Welcoming & Support Clients")
+st.subheader("Confirmation, Welcoming, Support and New Sales 💸")
 
